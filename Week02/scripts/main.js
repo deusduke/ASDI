df4503bc79b59640f3213078befdb0dff3d6619a
Deus Duke <deus@deusenterprises.com>
2013-06-13 21:03:12 -0500

    CRUD functionality implemented in JS

--- Week02/scripts/main.js
+++ Week02/scripts/main.js
@@ -6,6 +6,7 @@
 
 var jsonLoaded = false;
 var yamlLoaded = false;
+var createMode = false;
 
 lihtml = "<li class='' data-project-id='{0}'>{1}</li>";
 
@@ -19,25 +20,73 @@ $(function() {
 
     // initialze create
     $('#pgCreate').on('pageinit', function(){
-    	
     });
 
     // save project button clicked
     $("#btnGoCreate").click(function() {
         $('btnSave').text('Create');
+
+        $('form').submit();
+    });
+
+    // create / save project clicked
+    $('#btnSave').click(function() {
+        saveProject();
     });
 
 });
 
-function storeProject() {
+/**
+ * Validate the edit form, show / hide errors
+ * and return if there true / false if there are
+ * any problems
+ * @return {boolean}
+ */
+function validateForm() {
+    // TODO - Validate
 
+    return true;
 }
 
 /**
- * Use this to save an existing project
+ * Use this to save or update an existing project
  */
-function saveProject() {
+function saveProject(projectid) {
 
+    // make sure all data is valid, if not return
+    if (!validateForm()) return;
+
+    var p = {};
+
+    // create or store the id
+    if (projectid)
+        p.id = projectid;
+    else
+        p.id = Date.now().getTime();    // create id based off timestamp for guaranteed uniqueness
+
+    if (!projectid) {
+        // get vars
+        p.name = $('#name').val();
+        p.start_date = $('#start_date').val();
+        p.end_date = $('#end_date').val();
+        p.priority = $('#priority').val();
+        p.description = $('#description').val();
+
+        localStorage.setItem(p.id, JSON.stringify(p));
+    }
+
+    else {
+        // TODO
+    }
+}
+
+/**
+ * Get a project baseed off the id
+ * @param  {string/int} projectid
+ * @return {void}
+ */
+function getProject(projectid) {
+    return JSON.parse(localStorage.getItem(projectid));
 }
 
 /**
@@ -46,11 +95,23 @@ function saveProject() {
  */
 function getAllProjects() {
     var allProjects = [];
-    
+
     // pull all the data from storage
     for (var key in localStorage){
-        var p = localStorage.getItem(key);
-        
+        var p = getProject(key);
+
         allProjects.push(p);
     }
+
+    return allProjects;
+}
+
+/**
+ * Remove a project from local storage baseed off
+ * id
+ * @param  {int/string} projectid
+ * @return {void}
+ */
+function deleteProject(projectid) {
+    localStorage.removeItem(projectid);
 }
